3d9f1103f5b4cba7fffbd695139392b637549152
Arthur Selingin <arthur_santos@outlook.com.br>
2024-02-25 16:55:37 -0300

    criando script que pega a href de cada imagem cliclada para efeito de scroll

--- script.js
+++ script.js
@@ -1,4 +1,33 @@
-function changeimg() {
+const conteudoJogo = document.querySelectorAll('.gameContent'); /* pegando links*/ 
+
+console.log(conteudoJogo);
+
+
+    /* pegando cada elemento usando o argumento item */
+conteudoJogo.forEach(item =>{
+
+    /* fazendo evento click pegando o atributo do item */
+    item.addEventListener('click', scrollContent);
+
+    console.log(item);
+});
+
+
+    /*função que pega o atributo e retorna  */
+function scrollContent(event){
+    event.preventDefault();
+    const elemento = event.currentTarget;
+    const id = elemento.getAttribute('href');
+    const para = (id).offsetTop;
+
+    window.scroll({
+        top: para,
+        behavior: 'smooth'
+    });
+
+    console.log(elemento)
+    console.log(id)
+}
+
 
     
-}
