cc277d66000e384998952844ae93fa862cff38bc
Arthur Selingin <arthur_santos@outlook.com.br>
2024-02-26 08:43:53 -0300

    script bugado

--- script.js
+++ script.js
@@ -1,33 +1,32 @@
 const conteudoJogo = document.querySelectorAll('.gameContent'); /* pegando links*/ 
 
-console.log(conteudoJogo);
-
-
     /* pegando cada elemento usando o argumento item */
 conteudoJogo.forEach(item =>{
 
     /* fazendo evento click pegando o atributo do item */
     item.addEventListener('click', scrollContent);
-
-    console.log(item);
 });
 
-
-    /*função que pega o atributo e retorna  */
+/*função que pega o atributo e retorna  */
 function scrollContent(event){
-    event.preventDefault();
+
     const elemento = event.currentTarget;
-    const id = elemento.getAttribute('href');
-    const para = (id).offsetTop;
-
-    window.scroll({
-        top: para,
-        behavior: 'smooth'
-    });
-
     console.log(elemento)
+
+    const id = elemento.getAttribute('href');
+  
     console.log(id)
-}
+
+        const para = document.querySelector(id).offsetTop;
+
+        console.log(para)
+
+        para.scrollIntoView({behavior: "smooth"});
+    };
+
+
+
+
 
 
     
